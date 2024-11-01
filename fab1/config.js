5636648170a52a95c277e328c16c754d5109a3ef
Jose Dhonatas <dhontas12alves@gmail.com>
2024-10-31 21:59:23 -0300

    first commit

---
+++ fab1/config.js
@@ -0,0 +1,2 @@
+// config.js
+const GOOGLE_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSePs_LDZzNDJCib1uyk1K3PugbDadKPOiPcbtHeOpaSl9M1KQ/viewform?usp=pp_url";
---
+++ fab2/config.js
@@ -0,0 +1,2 @@
+// config.js
+const GOOGLE_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSe5LBXX6MMNTjU0uBwMB2i1pBgIsHzEOcM-e303EjDEai3GhQ/viewform?usp=pp_url";
